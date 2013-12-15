a1441ac9a8e59ed2927b4ddafcf78394a3602f0d
Denis-Lebreil <denis.lebreil@gmail.com>
2013-12-15 08:47:35 +0100

    premieres classes de test

--- Tests/zzChatTest.php
+++ Tests/zzChatTest.php
@@ -1,17 +1,20 @@
 <?php
 class zzChatTest extends PHPUnit_Framework_TestCase
 {
-    public function testPushAndPop()
+	 protected function setUp()
+	 {
+	 
+	 }
+	 
+    public function test()
     {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
- 
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
- 
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
+
+	return false;
+    }
+    
+    protected function tearDown()
+    {
+    
     }
 }
 ?>
